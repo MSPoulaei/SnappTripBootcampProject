e85fd4ced4e65359f8bd47bfec8f58700de299ed
MSPoulaei <sadeghpoolaee@gmail.com>
2022-08-20 17:44:55 +0430

    migration to postgre sql

--- SnappTrip/Program.cs
+++ SnappTrip/Program.cs
@@ -2,6 +2,8 @@ using SnappTrip.BusinessLayer.Interfaces;
 using SnappTrip.BusinessLayer.Services;
 using Microsoft.EntityFrameworkCore;
 using SnappTrip;
+using System.Net;
+using SnappTrip.DataAccessLayer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +14,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string connectionString = builder.Configuration.GetConnectionString("WebApiDatabase") ?? "Server=db;Database=SnappTripDB;User=sa;Password=Your_password123;";
-if (connectionString == "") connectionString = "Server=db;Database=SnappTripDB;User=sa;Password=Your_password123;";
-builder.Services.AddDbContext<SnappTrip.DataAccessLayer.SnappTripDbContext>(x => x.UseSqlServer(connectionString));
+//string connectionString = builder.Configuration.GetConnectionString("WebApiDatabase") ?? "Server=db;Database=SnappTripDB;User=sa;Password=Your_password123;";
+//string connectionString = $"Server={DockerHostMachineIpAddress},1433;Database=SnappTripDB;User=sa;Password=Your_password123;";
+string connectionString = "host=db;port=5432;database=SnappTripDB;username=sa;password=Your_password123";
+//builder.Services.AddDbContext<SnappTrip.DataAccessLayer.SnappTripDbContext>(x => x.UseSqlServer(connectionString));
+builder.Services.AddDbContext<SnappTripDbContext>(x => x.UseNpgsql(connectionString));
 builder.Services.AddTransient<IPopulateRepos, PopulateRepositoryHomeMadeCache>();
 builder.Services.AddTransient<IApplyRepos, ApplyRepository>();
 
